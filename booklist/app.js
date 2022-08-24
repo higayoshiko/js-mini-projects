b6052ed4ed5a2e5dc9f399d5cf3692e5046ef949
higayoshiko <higayoshiko@yahoo.com>
2022-08-24 13:08:52 +0900

    create store class and add functions

--- booklist/app.js
+++ booklist/app.js
@@ -8,15 +8,8 @@ class Book {
 
 class UI {
   static displayBooks(){
-    const StoredBooks = [
-      {
-        title: "one",
-        author: "one",
-        number: "one"
-      }
-    ];
 
-    const books = StoredBooks;
+    const books = Store.getBooks();
 
     books.forEach((book) => UI.addBookToList(book));
   }
@@ -42,10 +35,56 @@ class UI {
     }
   }
 
+  static showAlert(message, className) {
+    const div = document.createElement("div");
+    div.className = `alert alert-${className}`;
+    div.appendChild(document.createTextNode(message));
+    const container = document.querySelector(".container");
+    const form = document.querySelector("#book-form");
+    container.insertBefore(div, form);
+
+    setTimeout(() => {
+      document.querySelector(".alert").remove();
+    }, 3000);
+  }
+
   static clearFields() {
     document.querySelector("#title").value = "";
     document.querySelector("#author").value = "";
     document.querySelector("#number").value = "";
+  }
+}
+
+class Store {
+  static getBooks() {
+    let books;
+    if (localStorage.getItem("books") === null){
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem("books"));
+    }
+
+    return books;
+  }
+
+  static addBook(book) {
+    const books = Store.getBooks();
+
+    books.push(book);
+
+    localStorage.setItem("books", JSON.stringify(books));
+  }
+
+  static removeBook(number) {
+    const books = Store.getBooks();
+
+    books.forEach((book, index) => {
+      if(book.number === number){
+        books.splice(index, 1);
+      }
+    });
+
+    localStorage.setItem("books", JSON.stringify(books));
   }
 }
 
@@ -58,13 +97,24 @@ document.querySelector("#book-form").addEventListener("submit", (e) => {
   const author = document.querySelector("#author").value;
   const number = document.querySelector("#number").value;
 
-  const book = new Book(title, author, number);
+  if (title === '' || author === '' || number === ''){
+    UI.showAlert("fill in all fields", "danger");
 
-  UI.addBookToList(book);
+  } else {
+    const book = new Book(title, author, number);
 
-  UI.clearFields();
+    UI.addBookToList(book);
+
+    Store.addBook(book);
+
+    UI.showAlert("Book added", "success");
+
+    UI.clearFields();
+  }
 });
 
 document.querySelector("#book-list").addEventListener("click", (e) => {
   UI.deleteBook(e.target);
+  Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
+  UI.showAlert("Book removed", "success");
 });
